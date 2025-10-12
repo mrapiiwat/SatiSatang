7f41dff35967cda0789afbea1841d9f6478e4c94
Bahmimoodang <pockky15827@gmail.com>
2025-10-12 15:20:08 +0700

    1.เพิ่ม Component ปุ่มปฎิทินเข้ามา 2.ทำหน้า Main เมื่อ Login เข้ามาแล้ว โดยทำ 2.1 แสดงปุ่มปฏิทิน 2.2 ยอดใช้จ่าย 2.3 ปุ่มดูสรุปการเงิน

--- frontend/src/pages/users/Home.tsx
+++ frontend/src/pages/users/Home.tsx
@@ -1,32 +1,73 @@
-import useAuthStore from '../../store/authStore';
+// import useAuthStore from '../../store/authStore';
 import PageWrapper from '../../components/PageWrapper';
-import { FaApple } from 'react-icons/fa6';
 import FloatingBubble from '../../components/user/FloatingBubble';
+import MonthHeader from '../../components/user/MonthHeader';
+import { PiChartPieSliceLight } from "react-icons/pi";
+
+
+const mockTransactions = [
+  {
+    id: 1,
+    type: "income",
+    description: "รายรับจากเซเว่น",
+    amount: 1500,
+    createdAt: "2025-09-14T10:30:00Z",
+  },
+  {
+    id: 2,
+    type: "expense",
+    description: "ชาเขียวอิชิตัน",
+    amount: 40,
+    createdAt: "2025-09-14T15:00:00Z",
+  },
+  {
+    id: 3,
+    type: "expense",
+    description: "นายสุภเชต อิ่มบุญ",
+    amount: 190,
+    createdAt: "2025-09-12T18:00:00Z",
+  },
+];
 
 const Home = () => {
-  const token = useAuthStore((state) => state.token);
-  const user = useAuthStore((state) => state.user);
 
   const handleBubbleClick = () => {
     alert("เปิดแชท!");
   };
 
+  { /* แสดงรายจ่ายทั้งหมด */}
+  const totalExpense = mockTransactions
+    .filter((t) => t.type === "expense")
+    .reduce((sum, t) => sum + t.amount, 0);
+
   return (
     <PageWrapper animation="fade" duration={1.5}>
-      <div className="text-gray-500 break-words">token: {token}</div>
-      <div>id:{user?.id}</div>
-      <div>name:{user?.name}</div>
-      <div>email:{user?.email}</div>
-      <div>balance:{user?.balance}</div>
-      <div className="flex justify-center border-y border-gray-300 py-11 gap-7">
-        <FaApple size={80} />
-        <FaApple size={80} />
-        <FaApple size={80} />
-        <FaApple size={80} />
-        <FaApple size={80} />
-      </div>
-      <FloatingBubble onClick={handleBubbleClick} />
 
+      <div className="px-6 py-4 font-ibm text-black-900">
+
+        {/* เลือกเดือน */}
+          <MonthHeader />
+        {/* ยอดใช้จ่าย */}
+        <div className="text-center mb-6">
+          <p className="text-sm mb-4">ยอดใช้จ่าย</p>
+          <h1 className="text-[96px] mb-2 font-semibold leading-none">
+            {totalExpense.toLocaleString()}
+          </h1>
+
+          {/* ปุ่มดูสรุป */}
+          <button
+            className="flex items-center justify-center gap-2 bg-blue-600 hover:bg-blue-700 text-black-50 font-semibold text-sm rounded-[100px] w-[121px] h-[40px] transition mx-auto"
+          >
+            <PiChartPieSliceLight className="text-base" />
+            ดูสรุป
+          </button>
+        </div>
+
+        {/* รายการรายรับและรายจ่าย */}
+
+      </div>
+
+      <FloatingBubble onClick={handleBubbleClick} />
     </PageWrapper>
   );
 };
