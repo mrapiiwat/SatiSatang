92a4737a854e854a22387a10f1f51d49768ad920
Apiwat <mrapiiwat@gmail.com>
2026-05-09 22:04:13 +0700

    fix: update edit URL in docusaurus config to point to main branch

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mrapiiwat/SatiSatang/blob/develop/docs/',
+            'https://github.com/mrapiiwat/SatiSatang/blob/main/docs/',
         },
         blog: false,
         theme: {
